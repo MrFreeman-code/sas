ee135204d9dd793f49ce9ed581478c35df848d55
Didimus <didimus@mail>
2026-02-01 13:36:21 +0300

    create main config and include api router

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 # Аренда облачного сервиса: https://selectel.ru/services/cloud/servers/?section=about&utm_source=youtube.com&utm_medium=referral&utm_campaign=help_cloud_sharedline_13022024_shumeiko_paid
+import uvicorn
+from fastapi import FastAPI
+from api.routes import router as api_router
+from core.config import settings
 
-def main():
-    import uvicorn
-    from api.app import app, args
-
-    uvicorn.run(app, **args)
+app = FastAPI()
+app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+)
 
 
 if __name__ == "__main__":
-    main()
+    uvicorn.run("main:app",
+                host=settings.run.host,
+                port=settings.run.port,
+                reload=True)
