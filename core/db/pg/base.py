ed409f87444c53a7fb62a8d50d0e3dcbca320b28
Didimus <didimus@mail>
2026-02-14 16:55:05 +0300

    create DBSessionManager

--- core/db/pg/base.py
+++ core/db/pg/base.py
@@ -1,14 +1,48 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from pytest_asyncio.plugin import AsyncGenerator
+from sqlalchemy.ext.asyncio import (
+    create_async_engine, AsyncEngine, async_sessionmaker, AsyncSession)
 
-# URL для подключения к PostgreSQL
-DATABASE_URL = "postgresql://username:password@localhost/dbname"
+from core.config import settings
 
-# Создаём движок SQLAlchemy
-engine = create_engine(DATABASE_URL)
 
-# Создаём фабрику сессий для взаимодействия с БД
-Session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+class DBSessionManager:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        pool_size: int = 5,
+        max_overflow: int = 10
+        ):
+            """ Create async engine for connect in DB"""
+            self.engine: AsyncEngine = create_async_engine(
+                url=url,
+                echo=echo,
+                echo_pool=echo_pool,
+                pool_size=pool_size,
+                max_overflow=max_overflow
+            )
+            """ Create a session factory to get an AsyncSession for database operations"""
+            self.session_factory: async_sessionmaker[AsyncSession] = async_sessionmaker(
+                bind=self.engine,
+                autoflush=False,
+                autocommit=False,
+                expire_on_commit=False
+            )
+    async def dispose(self) -> None:
+        """ Close engine and connections"""
+        await self.engine.dispose()
 
-# Базовый класс для моделей
-Base = declarative_base()
+    async def session_getter(self) -> AsyncGenerator[AsyncSession, None]:
+        """ Create and close async session"""
+        async with self.session_factory() as session:
+            yield session
+
+
+db_session_manager = DBSessionManager(
+    url=str(settings.db.url),
+    echo=settings.db.echo,
+    echo_pool=settings.db.echo_pool,
+    pool_size=settings.db.pool_size,
+    max_overflow=settings.db.max_overflow
+)
