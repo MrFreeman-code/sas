b9a166321a3f20615823e2b2a158578f922d7544
Didimus <didimus@mail>
2026-02-14 16:55:25 +0300

    Fix: main

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 # Аренда облачного сервиса: https://selectel.ru/services/cloud/servers/?section=about&utm_source=youtube.com&utm_medium=referral&utm_campaign=help_cloud_sharedline_13022024_shumeiko_paid
 import uvicorn
 from fastapi import FastAPI
-from api.routes import router as api_router
 from core.config import settings
+from contextlib import asynccontextmanager
+from api.routes import router as api_router
+from core.db.pg.base import db_session_manager
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # strtup
+    yield
+    # shutdown
+    await db_session_manager.dispose()
+
+
+app = FastAPI(
+    lifespan=lifespan
+)
 app.include_router(
     api_router,
     prefix=settings.api.prefix
